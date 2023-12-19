6166bd39198608f27699aedfb9733e8ee2bff07f
DIVINO LUIZ DA COSTA JUNIOR <divino.iel@fieg.com.br>
2023-12-19 10:27:46 -0300

    bora galera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MainComponent } from './pages/main/main.component';
+import { SiderbarComponent } from './components/siderbar/siderbar.component';
+import { CardComponent } from './components/card/card.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PanelModule } from 'primeng/panel';
+import { CardModule } from 'primeng/card';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SiderbarComponent,
+    CardComponent,
+    NavBarComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SidebarModule,
+    ButtonModule,
+    BrowserAnimationsModule,
+    PanelModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/siderbar/siderbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-siderbar',
+  templateUrl: './siderbar.component.html',
+  styleUrls: ['./siderbar.component.scss']
+})
+export class SiderbarComponent {
+
+
+  sidebarVisible: boolean = false;
+
+  
+}
