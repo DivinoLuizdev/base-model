408f3e87830d8a8181cc845e2779de61be3c5239
Renato <renato.iel@fieg.com.br>
2024-01-05 16:13:26 -0300

    Atualização Sistema

---
+++ src/app/pages/emprestimos/emprestimos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-emprestimos',
+  templateUrl: './emprestimos.component.html',
+  styleUrls: ['./emprestimos.component.scss']
+})
+export class EmprestimosComponent {
+
+}
